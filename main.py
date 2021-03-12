fed11f7f9e2a59841c9c056670b5a98fec51481f
Satyam Raj <black4tiles@gmail.com>
2021-03-12 20:32:11 +0530

    birthday

---
+++ main.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+dvr = webdriver.Chrome()
+dvr.get("http://web.whatsapp.com/")
+
+num_of_msg = int(input('No. of wishes: '))
+
+input('Press any key: ')
+ 
+birthday_boy = dvr.find_element_by_xpath("//span[@title='rohit']")
+
+birthday_boy.click()
+
+msg_box = dvr.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
+
+for i in range(num_of_msg):
+    msg_box.send_keys("    ", i)
+    dvr.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button").click()
+
+print("Success")
