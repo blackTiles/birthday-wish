860a93a0049fa421f5cfe0806915c27cf618c3e2
Satyam Raj <77567242+blackTiles@users.noreply.github.com>
2021-03-12 20:37:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ num_of_msg = int(input('No. of wishes: '))
 
 input('Press any key: ')
  
-birthday_boy = dvr.find_element_by_xpath("//span[@title='rohit']")
+birthday_boy = dvr.find_element_by_xpath("//span[@title='whatsapp_username']")
 
 birthday_boy.click()
 
 msg_box = dvr.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
 
 for i in range(num_of_msg):
-    msg_box.send_keys("    ", i)
+    msg_box.send_keys("Happy Birthday", i)
     dvr.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button").click()
 
-print("Success")
+print("Chala Gaya")
